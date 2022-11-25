b0d35ea51005abdd73875256d2152b3062cc0d2c
Maziar Hassanzadeh <mhassanz@my.centennialcollege.ca>
2022-11-25 03:03:05 -0500

    Add project files.

---
+++ Config/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForecastApp.Config
+{
+    // name=maziar-api-weather, key=a10fb02279458d8a927d40755eb7170d
+
+    public static class Constants
+    {
+        public const string OPEN_WEATHER_APPID = "a10fb02279458d8a927d40755eb7170d";
+    }
+}
+
+//public const string OPEN_WEATHER_APPID = "2d716452512427c5fd5bbfe5262ea00f";
